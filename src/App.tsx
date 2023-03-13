3a9400f6b45e3a31cc84a6c0f6d3346a14074acb
Parahat <parahat.iljanow@gmail.com>
2023-03-13 16:33:53 +0500

    cd/cd - dev added

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React (PROD)</h1>
+      <h1>Vite + React (DEV)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
